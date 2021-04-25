a4a54f3788008df6a4467e37cc15d8fc5b28e3b2
sungpillhong <yous3163@naver.com>
2021-04-25 21:04:03 +0900

    주석 및 Datasource connection 확인

--- src/main/java/com/insight/board/configuration/DatabaseConfiguration.java
+++ src/main/java/com/insight/board/configuration/DatabaseConfiguration.java
@@ -11,6 +11,10 @@ import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
+/*
+  application.properties를 사용할 수 있게 설정 파일의 위치를 지정
+  @PropertySource를 이용하여 다른 설정파일도 사용가능함. 현재는 1개만..
+ */
 @Configuration
 @PropertySource("classpath:/application.properties")
 public class DatabaseConfiguration {
@@ -18,15 +22,22 @@ public class DatabaseConfiguration {
     @Autowired
     private ApplicationContext applicationContext;
 
+    /*
+      application.properties에 spring.datasource.hikari로 prifix를 정해놓아서
+     */
     @Bean
     @ConfigurationProperties(prefix = "spring.datasource.hikari")
     public HikariConfig hikariConfig(){
         return new HikariConfig();
     }
 
+    /*
+    앞에서 만든 히카리CP 설정 파일을 이용해 DB와 연결하는 dataSource 생성
+     */
     @Bean
     public DataSource dataSource() throws Exception{
         DataSource dataSource = new HikariDataSource(hikariConfig());
+        System.out.println("Datasource conntection:"+dataSource.toString());
         return dataSource;
     }
 
