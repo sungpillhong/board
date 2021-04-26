b4807f6de5009f290c195fb97fb7b1b143d9fe41
sungpillhong <yous3163@naver.com>
2021-04-26 23:38:13 +0900

    Dto 생성

---
+++ src/main/java/com/insight/board/dto/BoardDto.java
@@ -0,0 +1,20 @@
+package com.insight.board.dto;
+
+import lombok.Data;
+
+import java.util.Date;
+
+//@Data는 롬복의 어노테이션으로 모든 필드에 getter,setter,toString,hashCode,equals 메소드도 생성해줌
+@Data
+public class BoardDto { 
+    //DTO란 애플리케이션내의 각 계층 간 데이터를 주고 받는데 사용되는 객체
+    private int boardIdx;
+    private String title;
+    private String contents;
+    private int hitCnt;
+    private String creatorId;
+    private Date createdDatetime;
+    private String updaterId;
+    private Date updatedDatetime;
+    //t_board 테이블컬럼과 매칭되는 변수
+}
