af8ee15f80d55709a9d95564ff60a9c8ae0ddea0
sungpillhong <yous3163@naver.com>
2021-04-28 21:49:19 +0900

    Controller, Service, Mapper 생성

---
+++ src/main/java/com/insight/board/service/BoardService.java
@@ -0,0 +1,10 @@
+package com.insight.board.service;
+
+import com.insight.board.dto.BoardDto;
+
+import java.util.List;
+
+public interface BoardService {
+
+    List<BoardDto> selectBoardList() throws Exception;
+}
