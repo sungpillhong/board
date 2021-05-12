362b925b9f0950ea8738ef627e7bc6f8a679111b
sungpillhong <yous3163@naver.com>
2021-05-12 23:51:03 +0900

    병합중 오류 해결

--- src/main/java/com/insight/board/controller/BoardController.java
+++ src/main/java/com/insight/board/controller/BoardController.java
@@ -3,7 +3,9 @@ package com.insight.board.controller;
 import com.insight.board.dto.BoardDto;
 import com.insight.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
